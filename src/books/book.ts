5bdcadd1bae210e714178434aae4d50d4de6c63e
artmadeit <this.arthur.email@gmail.com>
2019-02-22 18:39:24 -0500

    feat(books): add initial properties

--- src/books/book.ts
+++ src/books/book.ts
@@ -1,1 +1,18 @@
-export class Book {}
+// interface Cover {
+//   small?: string;
+//   medium?: string;
+//   large?: string;
+// }
+
+export class Book {
+  id: number;
+  title: string;
+  subtitle: string;
+  numberOfPages: number;
+  // subjects
+  // publishers,
+  publishDate: Date;
+  // cover: Cover
+  // languages
+  // authors
+}
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Book } from './book';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  @Get()
+  findAll() {
+    const noSQLDistilled = new Book();
+    noSQLDistilled.id = 1;
+    noSQLDistilled.numberOfPages = 120;
+    noSQLDistilled.title = 'NoSQL distilled';
+    noSQLDistilled.publishDate = new Date(2016, 5, 2);
+    return [noSQLDistilled];
+  }
+}
