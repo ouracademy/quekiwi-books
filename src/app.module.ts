af5e5c56b6606dfec3bce10916696d2192557193
artmadeit <this.arthur.email@gmail.com>
2019-02-22 17:11:27 -0500

    feat: generate books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
