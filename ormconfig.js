17476586cc19b15ed55c59010a5a93dc549f4745
artmadeit <this.arthur.email@gmail.com>
2019-02-22 19:30:38 -0500

    refactor: extract ormconfig file

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'postgres',
+  host: 'stampy.db.elephantsql.com',
+  port: 5432,
+  username: 'qmpoukcp',
+  password: 'zDBGL6FI0dfzYRVzIQQc7wjPOlez4mvE',
+  database: 'qmpoukcp',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true
+};
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,16 +3,6 @@ import { createConnection } from 'typeorm';
 export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
-    useFactory: async () =>
-      await createConnection({
-        type: 'postgres',
-        host: 'stampy.db.elephantsql.com',
-        port: 5432,
-        username: 'qmpoukcp',
-        password: 'zDBGL6FI0dfzYRVzIQQc7wjPOlez4mvE',
-        database: 'qmpoukcp',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-  },
+    useFactory: async () => await createConnection()
+  }
 ];
