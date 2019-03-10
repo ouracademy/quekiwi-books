3753bba773269b93638ac52e6ad61955243f3ecb
qpdian <qpdiam@gmail.com>
2019-03-10 00:42:43 -0500

    feat: create book copie get user from req

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Param,
+  UseGuards,
+  createParamDecorator
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateUserBook } from './create-book-input';
+import { AuthGuard } from '@nestjs/passport';
+
+export const User = createParamDecorator((data, req) => {
+  return req.user;
+});
 
 @Controller('books')
 export class BooksController {
@@ -20,6 +34,11 @@ export class BooksController {
     return this.books.findByTitle(query.title);
   }
 
+  @Post(':id/copies')
+  @UseGuards(AuthGuard('jwt'))
+  addBookCopie(@Param('id') bookId, @User() user) {
+    return this.books.addBookCopie(bookId, user);
+  }
   @Post()
   create(@Body() input: CreateUserBook) {
     return this.books.create(input);
